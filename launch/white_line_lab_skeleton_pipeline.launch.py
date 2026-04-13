d520f8204a69fc2160843bb25df3f2f90b447968
Lunaris Elysium <rayzhang297@gmail.com>
2026-04-13 18:26:12 +0800

    add parameter

--- launch/white_line_lab_skeleton_pipeline.launch.py
+++ launch/white_line_lab_skeleton_pipeline.launch.py
@@ -20,7 +20,12 @@ def generate_launch_description():
             executable="white_line_lab_morph_node",
             name=morph_node_name,
             output="screen",
-            parameters=[{"input_topic": input_topic}],
+            parameters=[
+                {
+                    "input_topic": input_topic,
+                    "green_a_max": 117,
+                }
+            ],
         ),
         Node(
             package="rcj_localization",
@@ -35,7 +40,7 @@ def generate_launch_description():
                     "noise_mask_topic": f"/{morph_node_name}/noise_mask",
                     "orientation_window_radius_px": 5,
                     "min_orientation_neighbors": 6,
-                    "enable_image_view": False,
+                    "enable_image_view": True,
                 }
             ],
         ),
